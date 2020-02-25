55d00c0a8bd946d8906b9dcf5ea53d713da9aa3c
HoLLy <dario.db@hotmail.com>
2020-02-25 22:03:01 +0100

    Add implementations for new tracking methods

--- db.go
+++ db.go
@@ -32,10 +32,76 @@ func newSession(accessMode neo4j.AccessMode) neo4j.Session {
 }
 
 // should be able to return whether added or not :(
-func insertCapture(objUuid, camUuid, time string, posX, posY float32) error {
+func insertCapture(objUuid, camUuid string, time int32, posX, posY float32) error {
 	query := `MATCH (o:TracableObject{uuid:{objUuid}})
 	          MATCH (c:Camera{uuid:{camUuid}})
 	          CREATE (c)<-[:CAPTURED_BY]-(x:Capture{time:{time}, cameraX:{posX}, cameraY:{posY}})-[:CAPTURE_OF]->(o)`
 	variables := map[string]interface{}{"objUuid": objUuid, "camUuid": camUuid, "time": time, "posX": posX, "posY": posY}
 	return Write(query, variables)
+}
+
+type ObjectWithLastLocation struct {
+	Uuid, Name, Note string
+	LastLocation     Location
+}
+
+type Location struct {
+	X, Y, Z float32
+	Time    int32
+}
+
+func getAllObjects() ([]ObjectWithLastLocation, error) {
+	query := `MATCH (p:TrackingPoint)-[:TRACKS]->(t:TracableObject) WITH p, t
+	          ORDER BY p.time DESC WITH collect(p)[0] AS p, t
+	          RETURN t.uuid, t.name, t.note, p.x, p.y, p.z, p.time`
+	ret, err := Fetch(query, map[string]interface{}{}, func(result neo4j.Result) (interface{}, error) {
+		var ret []ObjectWithLastLocation
+		for result.Next() {
+			rec := result.Record()
+			ret = append(ret, ObjectWithLastLocation{
+				Uuid: rec.GetByIndex(0).(string),
+				Name: rec.GetByIndex(1).(string),
+				Note: rec.GetByIndex(2).(string),
+				LastLocation: Location{
+					X:    rec.GetByIndex(3).(float32),
+					Y:    rec.GetByIndex(4).(float32),
+					Z:    rec.GetByIndex(5).(float32),
+					Time: rec.GetByIndex(6).(int32),
+				},
+			})
+		}
+
+		return ret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return ret.([]ObjectWithLastLocation), nil
+}
+
+func getObjectHistory(uuid string) ([]Location, error) {
+	query := `MATCH (p:TrackingPoint)-[:TRACKS]->(:TracableObject{uuid:{uuid}}) WITH p
+	          ORDER BY p.time DESC
+	          RETURN p.x, p.y, p.z, p.time`
+	params := map[string]interface{}{"uuid": uuid}
+	ret, err := Fetch(query, params, func(result neo4j.Result) (interface{}, error) {
+		var ret []Location
+		for result.Next() {
+			rec := result.Record()
+			ret = append(ret, Location{
+				X:    rec.GetByIndex(0).(float32),
+				Y:    rec.GetByIndex(1).(float32),
+				Z:    rec.GetByIndex(2).(float32),
+				Time: rec.GetByIndex(3).(int32),
+			})
+		}
+
+		return ret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return ret.([]Location), nil
 }
--- server.go
+++ server.go
@@ -14,3 +14,46 @@ func (s server) AddCapture(_ context.Context, req *proto.AddCaptureRequest) (*pr
 	}
 	return &proto.AddCaptureReply{}, nil
 }
+
+func (s server) GetAllObjects(_ context.Context, req *proto.GetAllObjectsRequest) (*proto.GetAllObjectsReply, error) {
+	objects, err := getAllObjects()
+	if err != nil {
+		return nil, err
+	}
+
+	newObjects := make([]*proto.ObjectInfo, len(objects))
+	for i, e := range objects {
+		newObjects[i] = &proto.ObjectInfo{
+			Uuid: e.Uuid,
+			Name: e.Name,
+			Note: e.Note,
+			LastLocation: &proto.ObjectLocation{
+				X:    e.LastLocation.X,
+				Y:    e.LastLocation.Y,
+				Z:    e.LastLocation.Z,
+				Time: e.LastLocation.Time,
+			},
+		}
+	}
+
+	return &proto.GetAllObjectsReply{Objects: newObjects}, nil
+}
+
+func (s server) GetObject(_ context.Context, req *proto.GetObjectRequest) (*proto.GetObjectReply, error) {
+	history, err := getObjectHistory(req.Uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	newHistory := make([]*proto.ObjectLocation, len(history))
+	for i, e := range history {
+		newHistory[i] = &proto.ObjectLocation{
+			X:    e.X,
+			Y:    e.Y,
+			Z:    e.Z,
+			Time: e.Time,
+		}
+	}
+
+	return &proto.GetObjectReply{Locations: newHistory}, nil
+}
